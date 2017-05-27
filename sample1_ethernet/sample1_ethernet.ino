ff46196a78598fdd8f00836c666afe69b442ab7b
pavan <kavvuripavankumar@gmail.com>
2017-05-27 15:30:12 +0530

    My commit

---
+++ sample1_ethernet/sample1_ethernet.ino
@@ -0,0 +1,38 @@
+#if ARDUINO > 18
+#include <SPI.h> // needed for Arduino versions later than 0018
+#endif
+#include <Ethernet.h>
+byte mac[] = {  0A-00-27-00-00-00 };
+byte ip[] = { 175,101,67,191 }; // change to a valid address for your network
+byte server[] = { 64, 233, 187, 99 }; // Google
+// see text for more on IP addressing
+Client client(server, 80);
+void setup()
+{
+Ethernet.begin(mac, ip); // start ethernet using the mac and IP address
+Serial.begin(9600); // start the serial library:
+delay(1000); // give the ethernet hardware a second to initialize
+Serial.println("connecting...");
+if (client.connect()) {
+Serial.println("connected");
+client.println("GET /search?q=arduino HTTP/1.0"); // the HTTP request
+client.println();
+} else {
+Serial.println("connection failed");
+}
+}
+15.1 Setting Up the Ethernet Shield | 453
+www.it-ebooks.info
+void loop()
+{
+if (client.available()) {
+char c = client.read();
+Serial.print(c); // echo all data received to the Serial Monitor
+}
+if (!client.connected()) {
+Serial.println();
+Serial.println("disconnecting.");
+client.stop();
+for(;;)
+;
+}
---
+++ ultra_ethernet/ultra_ethernet.ino
@@ -0,0 +1,64 @@
+#include <SPI.h>
+#include <Ethernet.h>
+#define echoPin  2// Echo Pin
+#define trigPin 3 // Trigger Pin
+
+long duration; // Duration used to calculate distance
+
+byte mac[] = { 0x90, 0xA2, 0xDA, 0x0D, 0xFE, 0xD2 };
+IPAddress ip(192, 168, 0 ,109); 
+byte gateway[] = { 192,168,0,1 }; 
+
+byte server[] = { 192,168,0,104}; 
+
+byte subnet[]={255,255,255,0};
+EthernetClient client;
+char url[30];
+int val=40;
+void setup()
+{
+Serial.begin(9600);
+ pinMode(trigPin, OUTPUT);
+ pinMode(echoPin, INPUT);
+Ethernet.begin(mac, ip, gateway,subnet);
+}
+
+void loop()
+{
+  val=measure_water_level();
+
+  Serial.println(val);
+  //sprintf(url,"GET /tank.php?level=%d HTTP/1.1",val);
+  //Serial.println(url);
+  if (client.connect(server,80)) {  //starts client connection, checks for connection
+    sprintf(url,"GET /tank.php?level=%d HTTP/1.1",val);
+    Serial.println("connected");
+    client.println(url);
+    client.println( "Host: localhost");
+    client.println(); 
+   // delay(10);
+    client.stop();
+   } 
+  else {  
+    Serial.println("connection failed"); //error message if no client connect
+    Serial.println();
+   }
+  
+}
+
+int measure_water_level()
+{
+  digitalWrite(trigPin, LOW); 
+ delayMicroseconds(2); 
+
+ digitalWrite(trigPin, HIGH);
+ delayMicroseconds(10); 
+ 
+ digitalWrite(trigPin, LOW);
+ duration = pulseIn(echoPin, HIGH);
+
+float q=map(duration/29/2,0,22,100,0);
+//Serial.print("water level value");
+//Serial.println(q);
+  return q;
+}
